b05956d03c443e062a59c9a09e19d4b2892a523a
gavin <ob1.data@gmail.com>
2018-12-03 20:45:40 +1000

    One server - svr1

--- compute.tf
+++ compute.tf
@@ -1,69 +1,33 @@
 
-resource "aws_instance" "bastion" {
-  ami = "${var.bastion_ami_image}"
+resource "aws_instance" "svr1" {
+  ami = "${var.svr1_ami_image}"
   subnet_id = "${aws_subnet.pub_subnet.id}"
 
   tags{
-      Name="bastion_instance"
+      Name="first_instance"
   }
   # Use instance eligible for Free Tier
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.ingress-ssh.id}",
-                            "${aws_security_group.egress-all.id}",
-                            "${aws_security_group.ingress-ping.id}"]
+
   key_name = "${aws_key_pair.app_keypair.key_name}"
 }
 
 resource "aws_key_pair" "app_keypair" {
   public_key = "${file(var.public_keypair_path)}"
-  key_name = "bastion_kp"
+  key_name = "svr1_kp"
 }
 
-resource "aws_eip" "bastion-eip" {
+resource "aws_eip" "svr1-eip" {
   vpc = true
 }
 
-resource "aws_eip_association" "myapp_eip_assoc_bastion" {
-  instance_id = "${aws_instance.bastion.id}"
-  allocation_id = "${aws_eip.bastion-eip.id}"
+resource "aws_eip_association" "myapp_eip_assoc_svr1" {
+  instance_id = "${aws_instance.svr1.id}"
+  allocation_id = "${aws_eip.svr1-eip.id}"
 }
 
-output "bastion_elastic_ips" {
-  value = "${aws_eip.bastion-eip.public_ip}"
+output "svr1_elastic_ips" {
+  value = "${aws_eip.svr1-eip.public_ip}"
 }
 
-####################### General ###################################
-
-resource "aws_security_group" "ingress-ssh" {
-  name = "bastion_ingress-ssh"
-  vpc_id = "${aws_vpc.app_vpc.id}"
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "ingress-ping" {
-  name = "bastion_ingress-ping"
-  vpc_id = "${aws_vpc.app_vpc.id}"
-  ingress {
-    from_port   = 8
-    to_port     = 0
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "egress-all" {
-  name = "bastion_egress-all"
-  vpc_id = "${aws_vpc.app_vpc.id}"
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
--- variables.tf
+++ variables.tf
@@ -2,9 +2,8 @@
 variable "region_ip" {}
 variable "secret_key" {}
 variable "access_key" {}
-variable "bastion_ami_image" {}
-variable "bastion_user_name" {}
+variable "svr1_ami_image" {}
+variable "svr1_user_name" {}
 variable "public_keypair_path" {}
 variable "private_keypair_path" {}
-variable "packer_ami_image" {}
-variable "packer_user_name" {}
+
