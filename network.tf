830f2fe01fcc95f1444f1671a6c57040a9ce82b9
gavin <ob1.data@gmail.com>
2018-12-04 15:28:10 +1000

    Remove  default route table

--- network.tf
+++ network.tf
@@ -34,15 +34,6 @@ resource "aws_internet_gateway" "app_igw" {
   vpc_id = "${aws_vpc.app_vpc.id}"
 }
 
-resource "aws_default_route_table" "default_route" {
-  default_route_table_id = "${aws_vpc.app_vpc.default_route_table_id}"
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.app_igw.id}"
-  }
-}
-
 resource "aws_vpc_dhcp_options" "dns_resolver" {
   domain_name_servers = ["8.8.8.8", "8.8.4.4"]
 }
